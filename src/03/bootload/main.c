113674c1d57047bd8ebbb260d948705e7983a63b
pandayryr <knp.panda@gmail.com>
2023-02-12 16:25:27 +0900

    プログラム起動時に、ROM上の静的変数の初期値をRAM上にコピーする

--- src/03/bootload/main.c
+++ src/03/bootload/main.c
@@ -2,9 +2,22 @@
 #include "serial.h"
 #include "lib.h"
 
+static int init(void)
+{
+    /* リンカ・スクリプトで定義したシンボル */
+    extern int erodata, data_start, edata, bss_start, ebss;
+
+    /* データ領域とBSS領域を初期化する */
+    //&data_start: RAMの.dataセクションの先頭
+    //&erodata： ROMの.dataセクションの先頭 = .rodataの終端(.rodata → AT>.dataという配置になっているから)
+    memcpy(&data_start, &erodata, (long)&edata - (long)&data_start);
+    memset(&bss_start, 0, (long)&ebss - (long)&bss_start);
+
+    serial_init(SERIAL_DEFAULT_DEVICE);
+}
+
 int main(void)
 {
-    serial_init(SERIAL_DEFAULT_DEVICE);
     puts("Hello world!\n");
 
     putxval(0x10, 0);   puts("\n");
