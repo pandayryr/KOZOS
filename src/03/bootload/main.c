2bb5b8594a7cde9c7677ea240e542fc47961639f
pandayryr <knp.panda@gmail.com>
2023-02-12 16:47:40 +0900

    静的変数を操作し、出力するようにする

--- src/03/bootload/main.c
+++ src/03/bootload/main.c
@@ -14,14 +14,36 @@ static int init(void)
     memset(&bss_start, 0, (long)&ebss - (long)&bss_start);
 
     serial_init(SERIAL_DEFAULT_DEVICE);
+
+    return 0;
+}
+
+int global_data = 0x10;
+int global_bss;
+static int static_data = 0x20;
+static int static_bss;
+
+static void printval(void)
+{
+    puts("global_data = "); putxval(global_data, 0); puts("\n");
+    puts("global_bss = "); putxval(global_bss, 0); puts("\n");
+    puts("static_data = "); putxval(static_data, 0); puts("\n");
+    puts("static_bss = "); putxval(static_bss, 0); puts("\n");
 }
 
 int main(void)
 {
+    init();
+
     puts("Hello world!\n");
 
-    putxval(0x10, 0);   puts("\n");
-    putxval(0xffff, 0); puts("\n");
+    printval();
+    puts("over write valiables.\n");
+    global_data = 0x20;
+    global_bss = 0x30;
+    static_data = 0x40;
+    static_bss = 0x50;
+    printval();
 
     while(1);
 
