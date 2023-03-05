1b0ed8e8d465871b40333fc264b2a42971a7a98a
pandayryr <knp.panda@gmail.com>
2023-03-05 16:47:49 +0900

    セグメントをメモリ上に展開する処理を追加

--- src/06/bootload/elf.c
+++ src/06/bootload/elf.c
@@ -72,31 +72,27 @@ static int elf_load_program(struct elf_header* header)
             continue;
         }
 
-        //確認用に出力
-        putxval(phdr->offset, 6);           puts(" ");
-        putxval(phdr->virtual_addr, 8);     puts(" ");
-        putxval(phdr->physical_addr, 8);    puts(" ");
-        putxval(phdr->file_size, 5);        puts(" ");
-        putxval(phdr->memory_size, 5);      puts(" ");
-        putxval(phdr->flags, 2);            puts(" ");
-        putxval(phdr->align, 2);            puts("\n");
+        //elfファイルの示すセグメントの物理アドレス領域に、セグメントの内容をコピーする。
+        memcpy((char*)phdr->physical_addr, (char*)header + phdr->offset, phdr->file_size);
+        //メモリサイズとファイルサイズの差分を0で埋める(初期値指定のない静的変数の領域)
+        memset((char*)phdr->physical_addr + phdr->file_size, 0,  phdr->memory_size - phdr->file_size);
     }
 
     return 0;
 }
 
-int elf_load(char* buf)
+char* elf_load(char* buf)
 {
     struct elf_header* header = (struct elf_header*)buf;
 
     if(elf_check(header) < 0)
     {
-        return -1;
+        return NULL;
     }
     if(elf_load_program(header) < 0)
     {
-        return -1;
+        return NULL;
     }
 
-    return 0;
+    return (char*)header->entry_point;
 }
